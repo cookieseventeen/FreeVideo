52514445817fa0179f61d43cbec4507d970328eb
Ben <cookieseventeen@gamil.com>
2021-05-02 21:01:56 +0800

    更新V2版本，重新規劃ＵＩ，版面改為黑色，減少耗電

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,80 @@
 <template>
-  <div class="home">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h1 class="mb-3 mt-3">請貼上你的播放清單</h1>
-        </div>
+  <div class="player" :style="{ height: fullHeight + 'px' }">
+    <div class="player__content">
+      <div class="player__video">
+        <youtube
+          class="youtube-video"
+          :video-id="videoId"
+          ref="youtube"
+          @playing="playing"
+          :resize="true"
+          :fitParent="true"
+          :player-vars="playerVars"
+          @ended="ended"
+        ></youtube>
       </div>
-      <div class="row pt-5">
-        <div class="col-md-12">
-          <button
-            type="button"
-            class="btn btn-warning"
-            :class="{ disabled: !loopMusic }"
-            @click="setLoop"
-          >
-            重複播放
-          </button>
-          <button
-            type="button"
-            class="btn btn-info"
-            :class="{ disabled: !randomMusic }"
-            @click="setRandom"
-          >
-            隨機播放
-          </button>
-          <button type="button" class="btn btn-info" @click="setVidoe">
-            下一首
-          </button>
-        </div>
-        <!--
-        <div class="col-md-12">
-          <b-input-group class="mt-3">
-            <template v-slot:append>
-              <b-input-group-text><strong class="text-danger">請輸入單一影片</strong></b-input-group-text>
-            </template>
-            <b-form-input v-model="inputLink" ></b-form-input>
-          </b-input-group>
-        </div>
-        -->
-        <div class="col-md-12">
-          <!-- Using components -->
-          <b-input-group prepend="Youtube 列表" class="mt-3">
-            <b-form-input v-model="playListLink"></b-form-input>
-            <b-input-group-append>
-              <b-button variant="info" @click="getListVideo"
-                >載入播放列表</b-button
+      <div class="player__list">
+        <div class="ctrl-bar">
+          <div class="row">
+            <div class="col-6">
+              <button
+                type="button"
+                class="btn ctrl-bar__btn"
+                v-if="prevPageToken"
+                @click="getListVideo(prevPageToken)"
+                title="上一頁"
               >
-            </b-input-group-append>
-          </b-input-group>
+                <i class="fas fa-chevron-left"></i>上一頁
+              </button>
+            </div>
+            <div class="col-6">
+              <button
+                type="button"
+                class="btn ctrl-bar__btn"
+                v-if="nextPageToken"
+                @click="getListVideo(nextPageToken)"
+                title="下一頁"
+              >
+                下一頁<i class="fas fa-chevron-right"></i>
+              </button>
+            </div>
+          </div>
         </div>
+        <ul class="youtube-list" v-if="videoList">
+          <li
+            v-for="(item, key) in videoList"
+            :key="key"
+            :class="[key === currentVideo ? 'active' : '']"
+            @click="setVidoe(key)"
+          >
+            <template v-if="item.snippet.thumbnails.default">
+              <div
+                class="youtube-list__img"
+                :style="{
+                  backgroundImage:
+                    'url(' + item.snippet.thumbnails.default.url + ')',
+                }"
+              >
+                <img
+                  :src="item.snippet.thumbnails.default.url"
+                  :alt="item.snippet.title"
+                />
+              </div>
+            </template>
+            <div class="youtube-list__text-info">
+              <div class="youtube-list__title">
+                {{ key }}-{{ item.snippet.title }}
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
+    </div>
+
+    <!--
+    <div class="container-fluid">
       <div class="row">
-        <div class="col-md-12" v-if="videoId">
+        <div class="col-md-8" v-if="videoId">
           <youtube
             :video-id="videoId"
             ref="youtube"
@@ -62,6 +85,52 @@
             @ended="ended"
           ></youtube>
         </div>
+        <div class="col-md-4"></div>
+      </div>
+    </div>
+    -->
+    <div class="player__bar">
+      <div class="col-md-8">
+        <b-input-group prepend="Youtube 列表">
+          <b-form-input v-model="playListLink"></b-form-input>
+          <b-input-group-append>
+            <b-button variant="info" @click="getListVideo"
+              >載入播放列表</b-button
+            >
+          </b-input-group-append>
+        </b-input-group>
+      </div>
+      <div class="col">
+        <button
+          type="button"
+          class="btn btn-warning"
+          :class="{ disabled: !loopMusic }"
+          @click="setLoop"
+          title="重複播放"
+        >
+          <i class="fas fa-undo"></i>重複播放
+        </button>
+      </div>
+      <div class="col">
+        <button
+          type="button"
+          class="btn btn-info"
+          :class="{ disabled: !randomMusic }"
+          @click="setRandom"
+          title="隨機播放"
+        >
+          <i class="fas fa-random"></i>隨機播放
+        </button>
+      </div>
+      <div class="col">
+        <button
+          type="button"
+          class="btn btn-info"
+          @click="setVidoe"
+          title="下一首"
+        >
+          <i class="fas fa-fast-forward"></i>下一首
+        </button>
       </div>
     </div>
   </div>
@@ -82,12 +151,16 @@ export default {
         rel: 0,
         playsinline: 0,
       },
+      videoListID: null,
+      videoListVideoID: null,
       videoList: [],
       videoListCounter: 0,
       currentVideo: null,
       randomMusic: true,
       loopMusic: false,
       videoId: null,
+      nextPageToken: "",
+      prevPageToken: "",
     };
   },
   methods: {
@@ -114,7 +187,7 @@ export default {
       }
       this.playVideo();
     },
-    getListVideo() {
+    getListVideo(pageTokenData) {
       const vm = this,
         playListLink = vm.playListLink,
         listReg =
@@ -134,16 +207,28 @@ export default {
       //判斷是不是列表，如果是取出列表ID向youtube查詢．
       if (playListLink.indexOf("list") != -1) {
         let listID = getYotubePlaylistId(playListLink);
-        const youtubeLink = `${process.env.VUE_APP_YOUTUBEAPIPATH}?part=snippet%2CcontentDetails&maxResults=50&playlistId=${listID}&key=${process.env.VUE_APP_YOUTUBEKEY}`;
 
+        let httpParams = {
+          part: "snippet,contentDetails",
+          playlistId: listID,
+          maxResults: 50,
+          key: process.env.VUE_APP_YOUTUBEKEY,
+          pageToken: pageTokenData,
+        };
+        console.log("httpParams", httpParams);
         vm.$http
-          .get(youtubeLink, {
+          .get(process.env.VUE_APP_YOUTUBEAPIPATH, {
             withCredentials: false,
+            params: httpParams,
           })
           .then((res) => {
-            //console.log(res);
+            console.log(res.data);
+
             vm.videoList = res.data.items;
             vm.videoListCounter = vm.videoList.length;
+            vm.nextPageToken = res.data.nextPageToken;
+            vm.prevPageToken = res.data.prevPageToken;
+
             if (!vm.videoId) {
               vm.setVidoe();
             } else {
@@ -165,7 +250,7 @@ export default {
     setRandom() {
       this.randomMusic = !this.randomMusic;
     },
-    setVidoe() {
+    setVidoe(orderNum = null) {
       let vm = this;
       //如果是亂數產生
       if (vm.videoId == null || vm.randomMusic == true) {
@@ -181,12 +266,19 @@ export default {
           vm.currentVideo = 0;
         }
       }
+      if (typeof orderNum === "number") {
+        console.log(orderNum);
+        vm.currentVideo = orderNum;
+      }
       vm.videoId = this.videoList[vm.currentVideo].snippet.resourceId.videoId;
     },
   },
   computed: {
     player() {
       return this.$refs.youtube.player;
+    },
+    fullHeight() {
+      return window.innerHeight;
     },
   },
   components: {},
@@ -212,4 +304,118 @@ export default {
     */
   },
 };
+function getVideoData() {}
 </script>
+
+<style lang="scss" scoped>
+$dark-color: #030303;
+
+.player {
+  position: relative;
+  background: #030303;
+  &__bar {
+    position: absolute;
+    background: lighten($color: $dark-color, $amount: 15);
+    height: 100px;
+    width: 100%;
+    left: 0;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .btn {
+      margin: 0 8px;
+      i {
+        margin-right: 4px;
+      }
+    }
+  }
+  &__content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 120px;
+    bottom: 0;
+  }
+  &__video {
+    position: absolute;
+    left: 0;
+    width: 64%;
+    height: 100%;
+    ::v-deep iframe {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100% !important;
+      height: 100% !important;
+    }
+  }
+  &__list {
+    position: absolute;
+    right: 0;
+    width: 30%;
+    height: 100%;
+    padding-top: 42px;
+    overflow: hidden;
+  }
+}
+.ctrl-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  &__btn {
+    color: #fff;
+    &:hover {
+      color: #fff;
+      text-shadow: 1px 1px 1px rgba(255, 255, 255, 1);
+    }
+  }
+}
+.youtube-list {
+  position: absolute;
+  overflow-y: auto;
+  overflow-x: hidden;
+  top: 42px;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  li {
+    display: flex;
+    padding: 8px;
+    cursor: pointer;
+    & + li {
+      margin-top: 8px;
+    }
+    &.active {
+      background: lighten($color: $dark-color, $amount: 15);
+    }
+  }
+  &__img {
+    flex: 1;
+    background-size: cover;
+    background-position: center center;
+
+    &::before {
+      content: "";
+      display: block;
+      padding-top: 56.25%;
+    }
+    img {
+      display: none;
+    }
+  }
+  &__text-info {
+    display: flex;
+    width: 80%;
+    padding: 0 15px;
+    justify-content: left;
+    align-items: center;
+  }
+  &__title {
+    color: #fff;
+    text-align: left;
+  }
+}
+</style>
+
